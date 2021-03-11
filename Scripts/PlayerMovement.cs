056294e145fd9c4d2c7483cf6246255e6063afbf
Scott Geilen <scottgeilen99@gmail.com>
2021-03-11 16:16:12 -0700

    Player can move

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -6,15 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 moveDirection;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Processing inputs
+        ProcessInputs();
+    }
+
+    void FixedUpdate() {
+        // Physics Calculations
+        Move();
+    }
+
+    void ProcessInputs() {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+    void Move() {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
